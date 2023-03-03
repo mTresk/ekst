ad6c6ffd84989254762aab6af81bb8a519829741
Maxim Tresk <djtresk@gmail.com>
2023-03-03 17:10:58 +0000

    Fix download again

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -298,7 +298,7 @@
                 <div class="documents__body">
                     @foreach($documents as $document)
                         <div animate class="documents__item document">
-                            <a href="{{ $document->url }}" download class="document__link">
+                            <a href="/storage/{{ $document->url }}" download class="document__link">
                                 <svg class="document__icon" width="50" height="34" viewBox="0 0 50 34" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
                                     <path
